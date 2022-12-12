4da7c51296f198d26ecd30d9ce3c369b3994a0e0
Christopher Tavolazzi <ctavolazzi@gmail.com>
2022-12-11 19:25:38 -0800

    Update with more choices

--- ChatGPT/buttknuckle-cya/terminal-version/app.py
+++ ChatGPT/buttknuckle-cya/terminal-version/app.py
@@ -1,4 +1,6 @@
-# Define a function to display the game introduction and instructions
+# Define a global dictionary to store the player's choices
+choices = {}
+
 def display_intro():
   print("Welcome to the choose-your-own-adventure RPG!")
   print("In this game, you will make choices that determine the outcome of the story.")
@@ -11,72 +13,102 @@ def start_game():
   print("2. Try to find shelter for the night")
   print("3. Attempt to find food")
   choice = input("What would you like to do? ")
+  handle_choice("start", choice)
 
-  # Handle the player's choice
-  if choice == "1":
-    option_1()
-  elif choice == "2":
-    option_2()
-  elif choice == "3":
-    option_3()
+# Define a function to handle the player's choice
+def handle_choice(key, choice):
+  # Store the choice in the dictionary
+  choices[key] = choice
+
+  # Check if the choice is valid
+  if not choice.isdigit():
+    print("Invalid choice. Please enter a number.")
+    return
+
+  choice = int(choice)
+
+  # Handle the player's choice based on the current game state
+  if key == "start":
+    if choice == 1:
+      print("You spend hours walking, looking for a way out of the forest.")
+    elif choice == 2:
+      print("You spend hours walking, looking for a place to shelter for the night.")
+    elif choice == 3:
+      print("You spend hours walking, looking for food.")
+    else:
+      print("Invalid choice. Please try again.")
+      start_game()
+  elif key == "left":
+    if choice == 1:
+      print("The path to the left leads to a river.")
+    elif choice == 2:
+      print("The path to the left leads to a cave.")
+    elif choice == 3:
+      print("The path to the left leads to a house.")
+    else:
+      print("Invalid choice. Please try again.")
+      handle_choice("left", input("What would you like to do? "))
+  elif key == "right":
+    if choice == 1:
+      print("The path to the right leads to a river.")
+    elif choice == 2:
+      print("The path to the right leads to a cave.")
+    elif choice == 3:
+      print("The path to the right leads to a house.")
+    else:
+      print("Invalid choice. Please try again.")
+      handle_choice("right", input("What would you like to do? "))
+  elif key == "river":
+    if choice == 1:
+      print("You drink the water from the river.")
+    elif choice == 2:
+      print("You swim across the river.")
+    elif choice == 3:
+      print("You build a raft to cross the river.")
+    else:
+      print("Invalid choice. Please try again.")
+      handle_choice("river", input("What would you like to do? "))
+  elif key == "cave":
+    if choice == 1:
+      print("You enter the cave.")
+    elif choice == 2:
+      print("You climb the cave.")
+    elif choice == 3:
+      print("You dig under the cave.")
+    else:
+      print("Invalid choice. Please try again.")
+      handle_choice("cave", input("What would you like to do? "))
+  elif key == "house":
+    if choice == 1:
+      print("You enter the house.")
+    elif choice == 2:
+      print("You climb the house.")
+    elif choice == 3:
+      print("You dig under the house.")
+    else:
+      print("Invalid choice. Please try again.")
+      handle_choice("house", input("What would you like to do? "))
   else:
     print("Invalid choice. Please try again.")
-    start_game()
+    # start_game()
 
-# Define functions for each of the possible choices
-def option_1():
-  print("You spend hours walking, looking for a way out of the forest.")
-  print("After a while, you come across a fork in the path.")
-  print("1. Take the path to the left")
-  print("2. Take the path to the right")
-  choice = input("What would you like to do? ")
-
-  if choice == "1":
-    print("The path to the left leads you to a clearing, where you find a small cabin.")
-    print("You are able to find shelter for the night and rest until morning.")
-  elif choice == "2":
-    print("The path to the right leads you deeper into the forest, where you become even more lost.")
-    print("You eventually succumb to the elements and perish.")
-  else:
-    print("Invalid choice. Please try again.")
-    option_1()
-
-def option_2():
-  print("You spend hours walking, looking for a place to shelter for the night.")
-  print("After a while, you come across a fork in the path.")
-  print("1. Take the path to the left")
-  print("2. Take the path to the right")
-  choice = input("What would you like to do? ")
-
-  if choice == "1":
-    print("The path to the left leads you to a clearing, where you find a small cabin.")
-    print("You are able to find shelter for the night and rest until morning.")
-  elif choice == "2":
-    print("The path to the right leads you deeper into the forest, where you become even more lost.")
-    print("You eventually succumb to the elements and perish.")
-  else:
-    print("Invalid choice. Please try again.")
-    option_2()
-
-def option_3():
-  print("You spend hours walking, looking for food.")
-  print("After a while, you come across a fork in the path.")
-  print("1. Take the path to the left")
-  print("2. Take the path to the right")
-  choice = input("What would you like to do? ")
-
-  if choice == "1":
-    print("The path to the left leads you to a clearing, where you find a small cabin.")
-    print("You are able to find shelter for the night and rest until morning.")
-  elif choice == "2":
-    print("The path to the right leads you deeper into the forest, where you become even more lost.")
-    print("You eventually succumb to the elements and perish.")
-  else:
-    print("Invalid choice. Please try again.")
-    option_3()
-
-# Call the functions to start the game
+#start the game
 display_intro()
 start_game()
 
-# Path: buttknuckle-cya/terminal-version/app.py
+# Display the player's choices
+print("You have made the following choices:")
+for key, value in choices.items():
+  print(key, value)
+
+# The output of the program is:
+# Welcome to the choose-your-own-adventure RPG!
+# In this game, you will make choices that determine the outcome of the story.
+# To make a choice, enter the number corresponding to the option you want to choose.
+# You find yourself in a dark forest, lost and alone.
+# 1. Look for a way out of the forest
+# 2. Try to find shelter for the night
+# 3. Attempt to find food
+# What would you like to do?
+
+# And the player continues making choices from there
